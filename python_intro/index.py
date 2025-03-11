41f336ddae808e1a09b53e39f9bf9d16a1235c2c
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-03-11 22:18:31 +0300

    Initial commit

--- python_intro/index.py
+++ python_intro/index.py
@@ -1,1 +1,15 @@
-print("Hello world")
+a,b,c = 12, 5.6 , "I am learning python"
+print(c)
+d = a + b
+print(d)
+
+# list is an ordered collection of similar r different types of items sepatarated by commas and enclosed with brackets[]
+# can hold different data types
+
+languages = ["python", "Java", "Javascript", "C++", 7 , True]
+print(languages)
+print(languages[5])
+print(languages[-5])
+print(type(languages[4]))  
+# Print the data type of the element at index 4
+
